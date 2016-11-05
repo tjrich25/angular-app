38e3162f80c561f127e37cffef876482b450e743
Travis <Travis@Traviss-MacBook-Pro.local>
2016-11-05 14:53:26 -0400

    updated crtl1 and view1

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = angular.module('myModule', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
   .when('/view1', {
-    controller: ctrlOne,
+    controller: 'ctrlOne',
     templateUrl: 'partials/view1.html'
   })
   .when('/view2', {
-    controller: ctrlTwo,
+    controller: 'ctrlTwo',
     templateUrl: 'partials/view2.html'
   })
   .otherwise({
--- js/ctrl1.js
+++ js/ctrl1.js
@@ -1,5 +1,16 @@
 var app = angular.module('myModule');
 
 app.controller('ctrlOne', function($scope){
+  $scope.flySparklePony = function(sparklePony){
+    var originalWord = sparklePony;
+    var reverseWord = originalWord.split('').reverses().join('');
 
+    if (originalWord === reverseWord){
+      $scope.result = "The word is a palindrome";
+    }
+  else {
+    $scope.result = "The word is not a palindrome";
+    }
+
+  };
 });
