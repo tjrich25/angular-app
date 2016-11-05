60d514a88d903ef6769878b444d1cc9d463b1893
Travis <Travis@Traviss-MacBook-Pro.local>
2016-11-05 15:17:08 -0400

    modified function

--- js/ctrl1.js
+++ js/ctrl1.js
@@ -3,12 +3,11 @@ var app = angular.module('myModule');
 app.controller('ctrlOne', function($scope){
   $scope.flySparklePony = function(sparklePony){
     var originalWord = sparklePony;
-    var reverseWord = originalWord.split('').reverses().join('');
+    var reverseWord = originalWord.split('').reverse().join('');
 
     if (originalWord === reverseWord){
       $scope.result = "The word is a palindrome";
-    }
-  else {
+    } else {
     $scope.result = "The word is not a palindrome";
     }
 
